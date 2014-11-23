74aab73b03edfc152c4e2279eab01b82a1d213a1
hollodotme <hw@hollo.me>
2014-11-23 18:50:00 +0100

    Added base session registry

---
+++ dist/SessionRegistry.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ *
+ * @author hollodotme
+ */
+
+namespace hollodotme\Utilities;
+
+/**
+ * Class SessionRegistry
+ *
+ * @package hollodotme\Utilities
+ */
+abstract class SessionRegistry
+{
+
+	/** @var array */
+	private $data;
+
+	/**
+	 * @param array $session
+	 */
+	public function __construct( array &$session )
+	{
+		session_start();
+		$this->data = &$session;
+	}
+
+	/**
+	 * @param string $key
+	 * @param mixed  $value
+	 */
+	final protected function setSessionValue( $key, $value )
+	{
+		$this->data[ $key ] = $value;
+	}
+
+	/**
+	 * @param string $key
+	 *
+	 * @return null|mixed
+	 */
+	final protected function getSessionValue( $key )
+	{
+		if ( $this->isSessionKeySet( $key ) )
+		{
+			return $this->data[ $key ];
+		}
+		else
+		{
+			return null;
+		}
+	}
+
+	/**
+	 * @param string $key
+	 *
+	 * @return bool
+	 */
+	final protected function isSessionKeySet( $key )
+	{
+		return isset($this->data[ $key ]);
+	}
+
+	/**
+	 * @param string $key
+	 */
+	final protected function unsetSessionValue( $key )
+	{
+		if ( $this->isSessionKeySet( $key ) )
+		{
+			unset($this->data[ $key ]);
+		}
+	}
+}
